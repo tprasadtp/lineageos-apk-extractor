947fd47cdc855b83ee0dcf4049555628f4de1db4
Prasad Tengse <code@prasadt.com>
2018-10-26 21:07:09 +0200

    fix build scripts

--- copy_files.py
+++ copy_files.py
@@ -37,7 +37,7 @@ log_console_handler.setFormatter(logging.Formatter('[ {levelname:8s} ] - {messag
 # Add the handlers to the logger
 log.addHandler(log_file_handler)
 log.addHandler(log_console_handler)
-os.chdir(os.path.dirname(__file__))
+
 # Copy Files from Mount Point /system
 
 def copy_release_files():
@@ -53,7 +53,7 @@ def copy_release_files():
         except OSError:
             log.critical("Failed to create %s directory.", RELEASE_DIR)
             raise OSError
-    if Path.exists(TRANSFER_JSON):
+    if os.path.isfile(TRANSFER_JSON):
         with open(TRANSFER_JSON) as f:
             transfer = json.load(f)
         for app, path in transfer.item():
@@ -69,7 +69,7 @@ def copy_release_files():
 
 def copy_metadata_files():
     log.info("Copying Metadata")
-    if Path.exists(METADATA_DIR):
+    if os.path.isdir(METADATA_DIR):
         try:
             log.info("Deleting already existing folder..")
             shutil.rmtree(METADATA_DIR)
@@ -97,7 +97,7 @@ def check_last_build():
     Sets coco.json to set control data for releases...
     """
     log.info("Checking last build date...")
-    if Path.exists('metadata.json') and Path.exists(RELEASES_JSON):
+    if os.path.isfile('metadata.json') and os.path.isfile(RELEASES_JSON):
         with open('metadata.json') as f:
             last_metadata = json.loads((f.read))
         with open(RELEASES_JSON) as r:
@@ -114,12 +114,14 @@ def check_last_build():
         global TAG
         TAG = current_build_tag
         log.info('Current Release Tag is %s',current_build_tag)
-        if current_build_date > last_build_date  and current_build_tag == last_build_tag:
+        if current_build_date > last_build_date  and current_build_tag != last_build_tag:
             log.info("This release %s is New. GH Pages and GH Releases will be enabled if on MASTER")
             with open(FLAGS_SCRIPT, 'w+') as flag_file:
                 flag_file.write('#!/usr/bin/env bash\nexport DEPLOY=true\nexport BUILD_TAG=' + current_build_tag + '\n')
         else:
             log.info("Release is already the latest.")
+            with open(FLAGS_SCRIPT, 'w+') as flag_file:
+                flag_file.write('#!/usr/bin/env bash\nexport DEPLOY=false\nexport BUILD_TAG=' + current_build_tag + '\n')
 
 
 def main():
