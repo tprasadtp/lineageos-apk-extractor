fe39958e7befd29287a790f85d2e0e3a122d4cb4
tprasadtp <code@prasadt.com>
2018-10-27 19:55:42 +0200

    fix script

--- travis-deploy-metadata.sh
+++ travis-deploy-metadata.sh
@@ -5,13 +5,12 @@
 # you can find it at the link below.
 # https://opensource.org/licenses/MIT
 set -e
-METADATA_BRANCH="gh-pages"
 #GIT_DIR="${HOME}/pages"
 # Checkout the branch
 echo "saving PWD"
 pushd $PWD
-echo "Cloning ${METADATA_BRANCH} branch"
-git clone --branch=${METADATA_BRANCH}    https://github.com/tprasadtp/lineageos-apk-extractor gh-deploy 2>&1 > /dev/null
+echo "Cloning gh-pages branch"
+git clone --branch=gh-pages    https://github.com/tprasadtp/lineageos-apk-extractor gh-deploy 2>&1 > /dev/null
 #git clone --branch=${METADATA_BRANCH}    https://github.com/tprasadtp/lineageos-apk-extractor.git gh-deploy
 # Update pages
 rsync -Ea ./metadata/ ./gh-deploy/
@@ -21,5 +20,5 @@ git add -A .
 git config user.name  "Valarie-CI-Bot (TravisCI)"
 git config user.email "${GH_EMAIL}"
 git commit --allow-empty -am "CI-Updated for build ${BUILD_TAG} - with TS - ${LOGFILE_TS}"
-git push -fq https://valarie-ci-bot:${GH_TOKEN}@github.com/tprasadtp/lineageos-apk-extractor ${METADATA_BRANCH} 2>&1 > /dev/null
+git push -fq https://valarie-ci-bot:${GH_TOKEN}@github.com/tprasadtp/lineageos-apk-extractor gh-pages 2>&1 > /dev/null
 popd
